fe6171ee300f72df68474eb3cdc67789a2b0c398
anannanur <anannaanu974@gmail.com>
2022-05-06 22:51:44 +0600

    find data from database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const query = require('express/lib/middleware/query');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -14,18 +15,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gdexo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('db connected');
-  client.close();
-});
+
+async function run() {
+    await client.connect();
+    const perfumeCollection = client.db('perfumeInventory').collection('perfume');
+
+    app.get('/perfume', async(req, res) => {
+        const query = {};
+        const cursor = perfumeCollection.find(query);
+        const perfumes = await cursor.toArray();
+        res.send(perfumes);
+    })
+
+    app.get('/perfume/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const perfume = await perfumeCollection.findOne(query);
+        res.send(perfume);
+    })
+}
+run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Running perfume inventory management system');
 });
 
-app.listen(port, ()=> {
-    console.log('Listeing to port',port);
+app.listen(port, () => {
+    console.log('Listeing to port', port);
 })
