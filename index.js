ea044d84ecc400cf989e3dea5ed08b99e0393585
anannanur <anannaanu974@gmail.com>
2022-05-07 11:21:25 +0600

    inserting data by post method

--- index.js
+++ index.js
@@ -20,18 +20,27 @@ async function run() {
     await client.connect();
     const perfumeCollection = client.db('perfumeInventory').collection('perfume');
 
-    app.get('/perfume', async(req, res) => {
+    // finding all data by get method 
+    app.get('/perfume', async (req, res) => {
         const query = {};
         const cursor = perfumeCollection.find(query);
         const perfumes = await cursor.toArray();
         res.send(perfumes);
     })
 
-    app.get('/perfume/:id',async(req,res)=>{
+    // finding a data by get method 
+    app.get('/perfume/:id', async (req, res) => {
         const id = req.params.id;
-        const query = {_id: ObjectId(id)};
+        const query = { _id: ObjectId(id) };
         const perfume = await perfumeCollection.findOne(query);
         res.send(perfume);
+    })
+
+    // inserting a data by POST method 
+    app.post('/perfume', async (req, res) => {
+        const newPerfume = req.body;
+        const result = await perfumeCollection.insertOne(newPerfume);
+        res.send(result);
     })
 }
 run().catch(console.dir);
