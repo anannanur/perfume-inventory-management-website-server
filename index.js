09d00156f3e9a9d91fde397aa649c52d344c0be9
anannanur <anannaanu974@gmail.com>
2022-05-08 13:41:10 +0600

    updated delete api and get api for my items page

--- index.js
+++ index.js
@@ -16,69 +16,54 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    await client.connect();
-    const perfumeCollection = client.db('perfumeInventory').collection('perfume');
-    const individualCollection = client.db('perfumeInventory').collection('individualItems')
+    try {
+        await client.connect();
+        const perfumeCollection = client.db('perfumeInventory').collection('perfume');
 
-    // get api for find all items 
-    app.get('/perfume', async (req, res) => {
-        const query = {};
-        const cursor = perfumeCollection.find(query);
-        const perfumes = await cursor.toArray();
-        res.send(perfumes);
-    })
+        // GET API for finding all items 
+        app.get('/perfume', async (req, res) => {
+            const query = {};
+            const cursor = perfumeCollection.find(query);
+            const perfumes = await cursor.toArray();
+            res.send(perfumes);
+        });
 
-    // get api for find an item 
-    app.get('/perfume/:id', async (req, res) => {
-        const email = req.query.email;
-        const query ={email: email};
-        // const id = req.params.id;
-        // const query = { _id: ObjectId(id) };
-        const perfume = await perfumeCollection.findOne(query);
-        res.send(perfume);
-    })
+        // GET API for finding an item 
+        app.get('/perfume/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const perfume = await perfumeCollection.findOne(query);
+            res.send(perfume);
+        });
 
-    // POST api for inserting an item 
-    app.post('/perfume', async (req, res) => {
-        const newPerfume = req.body;
-        const result = await perfumeCollection.insertOne(newPerfume);
-        res.send(result);
-    })
+        // POST api for inserting an item 
+        app.post('/perfume', async (req, res) => {
+            const newPerfume = req.body;
+            const result = await perfumeCollection.insertOne(newPerfume);
+            res.send(result);
+        });
 
-    // delete api for delete an item
-    app.delete('/perfume/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await perfumeCollection.deleteOne(query);
-        res.send(result);
-    })
+        // DELETE API for deleting an item
+        app.delete('/perfume/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await perfumeCollection.deleteOne(query);
+            res.send(result);
+        });
 
-    // individual collection api
+        // GET API for loading user selected items 
+        app.get('/myitem', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = await perfumeCollection.find(query);
+            const perfumes = await cursor.toArray();
+            res.send(perfumes);
+        });
 
-    // creating GET API for loading individual's item 
-    app.get('/myitem', async(req,res)=>{
-        const email = req.query.email;
-        const query ={email: email};
-        const cursor = individualCollection.find(query);
-        const items = await cursor.toArray(); 
-        res.send(items);
-    })
+    }
+    finally {
 
-
-    // create POST API for inserting individual's item 
-    app.post('/myitem',async(req,res)=>{
-        const myItem = req.body;
-        const result = await individualCollection.insertOne(myItem);
-        res.send(result);
-    })
-
-
-    app.delete('/myitem/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await individualCollection.deleteOne(query);
-        res.send(result);
-    })
+    }
 }
 run().catch(console.dir);
 
